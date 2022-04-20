12dc7282a5c70c3af9a8e61982372dee3ce7457b
P Chudzik <plchudzik@gmail.com>
2022-04-20 16:48:20 +0100

    footer bar done

--- plc-spotify-clone/src/reducer.js
+++ plc-spotify-clone/src/reducer.js
@@ -4,9 +4,9 @@ export const initialState = {
   playing: false,
   item: null,
   // remove and change to null after dev:
-  token: null,
-  // token:
-  // "BQD7kHCXbMlLvXy4dApatPajoOuJLvSHnCRU4Z1S2KGIldOAf9sbFEPD_YVmVqhFNvm4F_s152N29x3hu-4R895cggqQCnlMcOsFTYyZyhw53y-fMNo7lhoCdHv6154_Gw90NtYvoBQJ4hC1Oynnw--xF3vTYD3w57bTbo9qOHh_PxE-Fh7L",
+  // token: null,
+  token:
+    "BQD7kHCXbMlLvXy4dApatPajoOuJLvSHnCRU4Z1S2KGIldOAf9sbFEPD_YVmVqhFNvm4F_s152N29x3hu-4R895cggqQCnlMcOsFTYyZyhw53y-fMNo7lhoCdHv6154_Gw90NtYvoBQJ4hC1Oynnw--xF3vTYD3w57bTbo9qOHh_PxE-Fh7L",
 };
 
 const reducer = (state, action) => {
